ccfcc1ce9438f1d0dba56902ed35b953539feb50
rks101 <rajendra.kr.solanki@gmail.com>
2022-07-02 21:19:10 +0530

    logout and save username

--- pages/index.js
+++ pages/index.js
@@ -2,8 +2,22 @@ import Head from 'next/head'
 import { getPosts } from 'lib/data.js'
 import prisma from 'lib/prisma'
 import Posts from 'components/Posts'
+import { useSession } from 'next-auth/react'
+import { useRouter } from 'next/router'
 
 export default function Home({ posts }) {
+  const { data: session, status } = useSession()
+  const router = useRouter()
+  const loading = status === 'loading'
+
+  if (loading) {
+    return null
+  }
+
+  if (session && !session.user.name) {
+    router.push('/setup')
+  }
+
   return (
     <div>
       <Head>
@@ -11,6 +25,16 @@ export default function Home({ posts }) {
         <meta name='description' content='A great social network!' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
+      <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
+        <p>Reddit clone</p>
+        <p className='grow'></p>
+        <a
+          className='flex-l border px-4 font-bold rounded-full mb-1'
+          href={session ? '/api/auth/signout' : '/api/auth/signin'}
+        >
+          {session ? 'logout' : 'login'}
+        </a>
+      </header>
 
       <Posts posts={posts} />
     </div>
