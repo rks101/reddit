3471562c1af24f48cd7053a737a5a45962cb4b26
rks101 <rajendra.kr.solanki@gmail.com>
2022-07-04 10:25:17 +0530

    allow submit post with a session

--- pages/r/[subreddit].js
+++ pages/r/[subreddit].js
@@ -1,10 +1,19 @@
 import Link from 'next/link'
 import prisma from 'lib/prisma'
 import { getSubreddit, getPostsFromSubreddit } from 'lib/data.js'
-
+import { Router, useRouter } from 'next/router'
+import { useSession } from 'next-auth/react'
 import Posts from 'components/Posts'
 
 export default function Subreddit({ subreddit, posts }) {
+  const router = useRouter()
+  const { data: session, status } = useSession()
+  const loading = status === 'loading'
+
+  if (loading) {
+    return null
+  }
+
   if (!subreddit) {
     return <p className='text-center p-5'>Subreddit does not exist 😞</p>
   }
@@ -12,15 +21,27 @@ export default function Subreddit({ subreddit, posts }) {
   return (
     <>
         <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
-        <Link href={`/`}>
+          <Link href={`/`}>
             <a className='underline'>Home</a>
-        </Link>
-        <p className='grow'></p>
+          </Link>
+          <p className='grow'></p>
         </header>
         <header className='bg-black text-white h-12 flex pt-3 px-5 pb-2'>
-        <p className='text-center'>/r/{subreddit.name}</p>
-        <p className='ml-4 text-left grow'>{subreddit.description}</p>
+          <p className='text-center'>/r/{subreddit.name}</p>
+          <p className='ml-4 text-left grow'>{subreddit.description}</p>
         </header>
+        { // show create post only when we have a session
+        session && (
+          <div className='border border-3 border-black p-10  mx-20 my-10'>
+            <input
+              placeholder='Create post'
+              className='border-gray-800 border-2 p-4 w-full'
+              onClick={() => {
+                router.push(`/r/${subreddit.name}/submit`)
+              }}
+            ></input>
+          </div>
+        )}
       <Posts posts={posts} />
     </>
   )
